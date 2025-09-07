3ca1312da9749e105ac3bed42aef1276df66a901
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-09-07 21:23:50 +0330

    improve gRPC imp

--- src/services/BasketService/src/BasketService.Api/Application/Services/BasketGrpcService.cs
+++ src/services/BasketService/src/BasketService.Api/Application/Services/BasketGrpcService.cs
@@ -1,6 +1,7 @@
 using System;
 using BasketService.Api.Infrastructure.Data;
 using BasketService.Protos;
+using Google.Protobuf;
 using Grpc.Core;
 using static BasketService.Protos.BasketService;
 
@@ -46,7 +47,67 @@ namespace BasketService.Api.Application.Services
                 },
                 Item = item,
             });
+        }
 
+        public override async Task<CreateResponse> Create(CreateRequest request, ServerCallContext context)
+        {
+            // Data Validation
+            request.Item.Id = new Random().Next(1000, 9999);       
+            var item = database.Items.Insert(request.Item);
+            return await Task.FromResult(new CreateResponse
+            {
+                Status = new ServiceResult
+                {
+                    Success = true,
+                    Message = "success.",
+                },
+                Item = database.Items.FindOne(p => p.Id == request.Item.Id),
+            });
+        }
+
+        public override async Task<UpdateResponse> Update(UpdateRequest request, ServerCallContext context)
+        {
+            bool item = database.Items.Update(request.Item);
+            if (!item)
+                return await Task.FromResult(new UpdateResponse
+                {
+                    Status = new ServiceResult
+                    {
+                        Success = false,
+                        Message = "not found!",
+                    },
+                });
+            return await Task.FromResult(new UpdateResponse
+            {
+                Status = new ServiceResult
+                {
+                    Success = true,
+                    Message = "success.",
+                },
+                Item = request.Item,
+            });
+        }
+
+        public override async Task<DeleteResponse> Delete(DeleteRequest request, ServerCallContext context)
+        {
+            int result = database.Items.DeleteMany(p => p.Id == request.Id);
+            if (result == 0)
+                return await Task.FromResult(new DeleteResponse
+                {
+                    Status = new ServiceResult
+                    {
+                        Success = false,
+                        Message = "not found!",
+                    },
+                });
+            return await Task.FromResult(new DeleteResponse
+            {
+                Status = new ServiceResult
+                {
+                    Success = true,
+                    Message = "success.",
+                },
+            });
         }
     }
 }
