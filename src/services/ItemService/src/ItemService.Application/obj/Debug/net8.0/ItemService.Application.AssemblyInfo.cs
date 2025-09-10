7eeb8547fa9b762c88b09fe7fa17789fb3c50553
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-09-10 14:10:34 +0330

    Initial GraphQL project

--- src/services/ItemService/src/ItemService.Application/obj/Debug/net8.0/ItemService.Application.AssemblyInfo.cs
+++ src/services/ItemService/src/ItemService.Application/obj/Debug/net8.0/ItemService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItemService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad0a87a02a481852d2b8dcb8a3f2c57ccc5fa8bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+927c2e6f81760b65f923ba3de040014c86f2e7d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItemService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItemService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/services/clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+solutions=(
+  "BasketService/BasketService.sln"
+  "GrapqlService/GraphqlService.sln"
+  "ItemService/ItemService.sln"
+  "JwtService/JwtService.sln"
+)
+
+for sln in "${solutions[@]}"; do
+  if [ -f "$sln" ]; then
+    echo "Cleaning $sln ..."
+    dotnet clean "$sln"
+  else
+    echo "Solution file $sln not found!"
+  fi
+done
